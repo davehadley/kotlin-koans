c48205ce7c2c2d9bca2b43fe034af2267bf21d1b
David Hadley <d.r.hadley@warwick.ac.uk>
2018-01-22 23:07:09 +0000

    Fix bug in date comparator. Now 28 is solved.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,8 +2,10 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     internal val days = dayOfMonth + month*12 + year*365
-    override fun compareTo(other: MyDate): Int {
-        return this.days - other.days
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
 
