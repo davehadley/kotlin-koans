525422f7e466768f1e38b842d8d807dacf9bbde9
David Hadley <d.r.hadley@warwick.ac.uk>
2018-01-22 19:11:45 +0000

    Better solution for 4.

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -22,7 +22,4 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean {
-    collection.forEach { if (it % 2 == 0) return true }
-    return false
-}
+fun task4(collection: Collection<Int>) = collection.any { it % 2 == 0 }
