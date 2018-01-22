33f3e995c2f64122d84401cc348350c6e7236bda
David Hadley <d.r.hadley@warwick.ac.uk>
2018-01-22 22:31:35 +0000

    Solve 25.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,11 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    private val days = dayOfMonth + month*12 + year+365
+    override fun compareTo(other: MyDate): Int {
+        return this.days - other.days
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
